4c28b23a2f3e9781799c6f2a120d07e7dafddb24
lollcat <44415540+lollcat@users.noreply.github.com>
2019-10-06 13:21:25 +0200

    Updates

--- Discrete and Continuous/Hybrid_paper_TF/main.py
+++ Discrete and Continuous/Hybrid_paper_TF/main.py
@@ -11,10 +11,10 @@ agent = Agent(alpha=0.0001, beta=0.00001, input_dims=env.observation_space.shape
               env=env, batch_size=32, layer1_size=64, layer2_size=32, layer3_size=32,
               n_discrete_actions=env.discrete_action_space.n, n_continuous_actions=1)
 np.random.seed(0)
-total_eps = 20000
+total_eps = 500000
 total_eps_greedy = total_eps/2
 # if there is a saved agent then uncomment below:
-#agent.load_models()
+agent.load_models()
 
 score_history = []
 for i in range(total_eps):
@@ -36,20 +36,22 @@ for i in range(total_eps):
         print('episode ', i, 'score %.2f' % score,
           'trailing 100 games avg %.3f' % np.mean(score_history[-100:]))
 
-    if i % 1000 == 0 and i > 10:
+    if i % (total_eps/20) == 0 and i > 100:
         env.render()
         agent.save_models()
-
-    if i % (total_eps/10) == 0 and i > 100:
         plotter = Plotter(score_history, i)
         plotter.plot()
 
+plotter = Plotter(score_history, i)
+plotter.plot()
 done = False
 state = env.reset()
 while done is False: # run an episode
+    print(state)
     action = agent.best_action(state)
     action = action_continuous, action_discrete
     state, reward, done, info = env.step(action)
+
 
 print(env.sep_order)
 print(env.split_order)
